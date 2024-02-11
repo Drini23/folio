b8c539151f59779e86ec8f9bc478928c17de1d58
Drini23 <122325981+Drini23@users.noreply.github.com>
2024-02-11 16:52:05 +0200

    if

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')), 
 ] 
 
+
+# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
